5dc4289c6d1ec491534b6ad868d3dd22519d94b2
Joe Nyland <joenyland@me.com>
2014-01-08 22:00:31 +0000

    Maintenance page.

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ header('Retry-After: 3600');
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
     <head>
+        <meta name="robots" content="noindex" />
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title>Trichema</title>
