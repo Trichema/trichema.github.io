7dd27bee532762f4e70907ac8c33dab57195be22
Joe Nyland <joenyland@me.com>
2014-07-10 21:36:44 +0100

    Rewrite maintenance_page to remove PHP code

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+/**
+ * Project: trichema.co.uk
+ * File: main.js
+ * User: joenyland
+ * Date: 10/07/2014
+ * Time: 11:50
+ */
+
+function getYear() {
+    var d = new Date();
+    var n = d.getFullYear();
+    document.write(n.toString());
+}
